02000531bbb2fe139753793141c61cef59a3ba69
Maxime Laforet <me@macx.im>
2022-09-02 14:11:44 +0100

    Force refetch twitter og image (#8)
    * Fix og image link
    
    * twitter fixes
    
    * FIx meta
    
    * Force refetch

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
         <meta name="twitter:title" content="The best way to build and grow your decentralized community" />
 
         {/* Open Graph */}
-        <meta property="og:image" content="https://www.console.xyz/console-og.png" key="ogimage" />
+        <meta property="og:image" content="https://www.console.xyz/console-og.png?20220902" key="ogimage" />
         <meta property="og:title" content="The best way to build and grow your decentralized community" />
         <meta property="og:site_name" content="Console" />
         <meta property="og:url" content="https://www.console.xyz/" />
